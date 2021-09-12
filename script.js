2d91ab9352bbe28a6937676aaf7dd92dc750fa59
sushmamh7 <sushmamh732@gmail.com>
2021-09-12 20:25:42 +0530

    my first commit

--- script.js
+++ script.js
@@ -1,114 +1,94 @@
-const selectBox = document.querySelector(".select-box"),
-selectBtnX = selectBox.querySelector(".options .playerX"),
-selectBtnO = selectBox.querySelector(".options .playerO"),
-playBoard = document.querySelector(".play-board"),
-players = document.querySelector(".players"),
-allBox = document.querySelectorAll("section span"),
-resultBox = document.querySelector(".result-box"),
-wonText = resultBox.querySelector(".won-text"),
-replayBtn = resultBox.querySelector("button");
+let currentPlayer = "x";
 
-window.onload = ()=>{
-    for (let i = 0; i < allBox.length; i++) {
-       allBox[i].setAttribute("onclick", "clickedBox(this)");
+let gameStatus = "Game On";
+
+const boxes = document.getElementsByClassName("box");
+
+for (let i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener("click", function() {
+    if (boxes[i].innerHTML.trim() == "" && gameStatus == "Game On") {
+      boxes[i].innerHTML = currentPlayer;
+      currentPlayer = currentPlayer == "x" ? "o" : "x";
+      document.getElementById(
+        "player"
+      ).innerHTML = currentPlayer.toUpperCase();
+
+      if (
+        boxes[0].innerHTML == boxes[1].innerHTML &&
+        boxes[1].innerHTML == boxes[2].innerHTML &&
+        boxes[0].innerHTML.trim() != ""
+      ) {
+        showWinner(0, 1, 2);
+      } else if (
+        boxes[3].innerHTML == boxes[4].innerHTML &&
+        boxes[4].innerHTML == boxes[5].innerHTML &&
+        boxes[3].innerHTML.trim() != ""
+      ) {
+        showWinner(3, 4, 5);
+      } else if (
+        boxes[6].innerHTML == boxes[7].innerHTML &&
+        boxes[7].innerHTML == boxes[8].innerHTML &&
+        boxes[6].innerHTML.trim() != ""
+      ) {
+        showWinner(6, 7, 8);
+      } else if (
+        boxes[0].innerHTML == boxes[3].innerHTML &&
+        boxes[3].innerHTML == boxes[6].innerHTML &&
+        boxes[0].innerHTML.trim() != ""
+      ) {
+        showWinner(0, 3, 6);
+      } else if (
+        boxes[1].innerHTML == boxes[4].innerHTML &&
+        boxes[4].innerHTML == boxes[7].innerHTML &&
+        boxes[1].innerHTML.trim() != ""
+      ) {
+        showWinner(1, 4, 7);
+      } else if (
+        boxes[2].innerHTML == boxes[5].innerHTML &&
+        boxes[5].innerHTML == boxes[8].innerHTML &&
+        boxes[2].innerHTML.trim() != ""
+      ) {
+        showWinner(2, 5, 8);
+      } else if (
+        boxes[0].innerHTML == boxes[4].innerHTML &&
+        boxes[4].innerHTML == boxes[8].innerHTML &&
+        boxes[0].innerHTML.trim() != ""
+      ) {
+        showWinner(0, 4, 8);
+      } else if (
+        boxes[2].innerHTML == boxes[4].innerHTML &&
+        boxes[4].innerHTML == boxes[6].innerHTML &&
+        boxes[2].innerHTML.trim() != ""
+      ) {
+        showWinner(2, 4, 6);
+      }
     }
+  });
 }
 
-selectBtnX.onclick = ()=>{
-    selectBox.classList.add("hide");
-    playBoard.classList.add("show");
-}
+//resetting the game
+document.getElementById("reset").addEventListener("click", function() {
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML = "";
+    boxes[i].style.backgroundColor = "#dee9ec";
+    boxes[i].style.color = "black";
+  }
+  currentPlayer = "x";
+  document.getElementById("message").style.display = "none";
+  document.getElementById("player").innerHTML = "X";
+  gameStatus = "Game On";
+});
 
-selectBtnO.onclick = ()=>{ 
-    selectBox.classList.add("hide");
-    playBoard.classList.add("show");
-    players.setAttribute("class", "players active player");
-}
-
-let playerXIcon = "fas fa-times",
-playerOIcon = "far fa-circle",
-playerSign = "X",
-runBot = true;
-
-function clickedBox(element){
-    if(players.classList.contains("player")){
-        playerSign = "O";
-        element.innerHTML = `<i class="${playerOIcon}"></i>`;
-        players.classList.remove("active");
-        element.setAttribute("id", playerSign);
-    }else{
-        element.innerHTML = `<i class="${playerXIcon}"></i>`;
-        element.setAttribute("id", playerSign);
-        players.classList.add("active");
-    }
-    selectWinner();
-    element.style.pointerEvents = "none";
-    playBoard.style.pointerEvents = "none";
-    let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed();
-    setTimeout(()=>{
-        bot(runBot);
-    }, randomTimeDelay);
-}
-
-function bot(){
-    let array = [];
-    if(runBot){
-        playerSign = "O";
-        for (let i = 0; i < allBox.length; i++) {
-            if(allBox[i].childElementCount == 0){
-                array.push(i);
-            }
-        }
-        let randomBox = array[Math.floor(Math.random() * array.length)];
-        if(array.length > 0){
-            if(players.classList.contains("player")){ 
-                playerSign = "X";
-                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
-                allBox[randomBox].setAttribute("id", playerSign);
-                players.classList.add("active");
-            }else{
-                allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
-                players.classList.remove("active");
-                allBox[randomBox].setAttribute("id", playerSign);
-            }
-            selectWinner();
-        }
-        allBox[randomBox].style.pointerEvents = "none";
-        playBoard.style.pointerEvents = "auto";
-        playerSign = "X";
-    }
-}
-
-function getIdVal(classname){
-    return document.querySelector(".box" + classname).id;
-}
-function checkIdSign(val1, val2, val3, sign){ 
-    if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
-        return true;
-    }
-}
-function selectWinner(){
-    if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6, playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7, playerSign) || checkIdSign(2,5,8, playerSign) || checkIdSign(3,6,9, playerSign) || checkIdSign(1,5,9, playerSign) || checkIdSign(3,5,7, playerSign)){
-        runBot = false;
-        bot(runBot);
-        setTimeout(()=>{
-            resultBox.classList.add("show");
-            playBoard.classList.remove("show");
-        }, 700);
-        wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`;
-    }else{
-        if(getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != ""){
-            runBot = false;
-            bot(runBot);
-            setTimeout(()=>{
-                resultBox.classList.add("show");
-                playBoard.classList.remove("show");
-            }, 700);
-            wonText.textContent = "Match has been drawn!";
-        }
-    }
-}
-
-replayBtn.onclick = ()=>{
-    window.location.reload();
+//displays the winner
+function showWinner(x, y, z) {
+  boxes[x].style.background = "#0d8b70";
+  boxes[x].style.color = "white";
+  boxes[y].style.background = "#0d8b70";
+  boxes[y].style.color = "white";
+  boxes[z].style.background = "#0d8b70";
+  boxes[z].style.color = "white";
+  document.getElementById("winner").innerHTML =
+    currentPlayer == "x" ? "O" : "X";
+  document.getElementById("message").style.display = "block";
+  gameStatus = "Game Over";
 }
